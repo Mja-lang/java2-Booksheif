15f7ca1da8b568b37d2380c5c4d0105ad8ee17d0
Asa Jesus Armstrong <k0755017@kirkwood.edu>
2021-11-04 20:57:38 -0500

    made arraylist of books

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.sql.SQLOutput;
+import java.util.*;
 
 public class Main {
 
@@ -13,7 +14,17 @@ public class Main {
         Book b1 = new Book("Java Generics", "Mujahid");
         Book b2 = new Book("Java Collections", "Mujahid");
         Book b3 = new Book("Java Collections", "asa");
-	// write your code here
+
+
+        List<Book> bookshelf = new ArrayList<>();
+        bookshelf.add(b1);
+        bookshelf.add(b2);
+        bookshelf.add(b3);
+        bookshelf.add(0, b4);
+
+        for (int i = 0; i < bookshelf.size(); i++){
+            System.out.println(bookshelf.get(i));
+        }
 
     }
 }
