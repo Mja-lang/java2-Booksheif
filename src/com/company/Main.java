3838c80ed501bba3b3e5144f11b3bd179744a809
Mujahid segyer <mjahdkhlyl85@gmail.com>
2021-11-04 20:49:41 -0500

    Add Book class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+        Book b1 = new Book("Java Generics", "Mujahid");
+        Book b2 = new Book("Java Collections", "Mujahid");
+        Book b3 = new Book("Java Collections", "asa");
 	// write your code here
     }
 }
